f33e517e2e3b67c76970d13084093b230d8b5519
rotess <chanhae1020@gmail.com>
2019-09-27 15:25:11 +0900

    Hello,NumberPrint 수정

--- Hello.java
+++ Hello.java
@@ -3,5 +3,7 @@ public static void main(String[] args){
 	System.out.print("Hello World");
 System.out.print("안녕하세요");
 
+System.out.print("Good bye");
+System.out.print("졸려");
 }
 }
--- numberprint.java
+++ numberprint.java
@@ -1,7 +1,9 @@
 public class NumberPrint{
 public static void main(String[] atgs){
 for(int i=0;i<=10;<i++){
-System.out.print(i+" ");
-}
+System.out.print(i+" ")
+
+	;
+}System.out.printLn();
 }
 }
